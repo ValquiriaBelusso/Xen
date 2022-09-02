96084d6e7388b90b678932781e40404f837354bf
Valquíria <51461327+ValquiriaBelusso@users.noreply.github.com>
2022-09-02 13:28:19 -0300

    Add files via upload

--- cpu.sh
+++ cpu.sh
@@ -11,9 +11,16 @@ cpu_threshold='80'
 #---cpu
 cpu_usage () {
 cpu_idle=`top -b -n 1 | grep 'Cpu' | awk '{print $8}'|cut -f 1 -d ","`
+
+if [ $cpu_idle = "id" ]
+   then cpu_idle=0
+fi
+
 cpu_use=`expr 100 - $cpu_idle`
 cpu_online=`nproc`
 ncpu=`expr $cpu_online + 1`
+
+echo "$(date +%F_%T) USE: $cpu_use CPUS: $ncpu" >> cpu_log.dat
 
 echo "cpu utilization: $cpu_use"
 echo "cpus online: $cpu_online" 
@@ -22,7 +29,7 @@ echo "cpus online: $cpu_online"
 if [ $cpu_use -gt $cpu_threshold ]
     then
         echo "cpu warning!"
-        sudo xenstore-write -s /local/domain/1/cpu/$ncpu/availability online 
+        sudo xenstore-write -s /local/domain/3/cpu/$ncpu/availability online 
                 
     else
         echo "cpu ok!"
@@ -31,5 +38,5 @@ fi
 
 while : ; do
 cpu_usage
-sleep 50
+sleep 3
 done
